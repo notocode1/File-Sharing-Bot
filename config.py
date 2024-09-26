83a1aa64c48d33fa077962f6473ea8957f4a0fc2
notocode1 <Abaenezersos@gmail.com>
2024-09-26 20:09:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,67 +1,41 @@
-#(©)CodeXBotz
-
-
-
-
 import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-
-
-#Bot token @Botfather
+# API Configuration
+API_HASH = os.environ.get('API_HASH', None)
+APP_ID = int(os.environ.get('APP_ID', 0))
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+OWNER_ID = int(os.environ.get("OWNER_ID", "12345678"))
 
-#Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+# Channel ID
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "0"))
 
-#Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+# Adding three forced subscription channels
+FORCE_SUB_CHANNEL_1 = 'YourFirstChannelID'
+FORCE_SUB_CHANNEL_2 = 'YourSecondChannelID'
+FORCE_SUB_CHANNEL_3 = 'YourThirdChannelID'
 
-#Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
-
-#OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-
-#Port
-PORT = os.environ.get("PORT", "8080")
-
-#Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
-
-#force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
-
+# Bot workers
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-#start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
-try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
-        ADMINS.append(int(x))
-except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
+# Start message
+START_MSG = os.environ.get("START_MESSAGE", "Hello {{first}}\\n\\nI can store private files in Specified Channel and other users can access it from special link.")
 
-#Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+# Force sub message 
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {{first}}\\n\\n<b>You need to join in my Channel/Group to use me\\n\\nKindly Please join Channel</b>")
 
-#set your Custom Caption here, Keep None for Disable Custom Caption
+# Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
-#set True if you want to prevent users from forwarding files from bot
+# Protect content
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
-#Set true if you want Disable your Channel Posts Share button
+# Disable Channel Button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
-BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
-
-ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+BOT_STATS_TEXT = "<b>BOT UPTIME</b>\\n{{uptime}}"
+USER_REPLY_TEXT = "❌ Don't send me messages directly I'm only a File Share bot!"
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
@@ -78,8 +52,8 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
